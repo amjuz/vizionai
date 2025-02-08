f786762baa2cb345fbc86e70999084bedf23f7b8
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-02-08 07:34:58 +0530

    chore: added stripe migration files, updated database types

--- src/app/(dashboard)/billing/page.tsx
+++ src/app/(dashboard)/billing/page.tsx
@@ -1,7 +1,15 @@
 
 const page = () => {
   return (
-    <div>billing page</div>
+    <section className="container mx-auto space-y-8">
+      <div className="">
+        <h1 className="text-3xl font-bold tracking-tight">Plans & Billing</h1>
+        <p className="text-muted-foreground">
+          Manage your Billing and Subscription information
+        </p>
+
+      </div>
+    </section>
   )
 }
 
